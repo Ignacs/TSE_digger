62346be0ed26f5fa03f9b4eeabd6216a9109dea9
Ignacs <seellk@gmail.com>
2015-05-25 00:57:33 +0800

    1. modify output encode format 2. remove debug info

--- dw_data_parser.py
+++ dw_data_parser.py
@@ -25,29 +25,36 @@ for i in csv.reader(f):
 		sid=str(i[0])
 
 		if sid.isdigit():
-			print i[0], 
+		#	print i[0], 
 			# print stock name in chinese
 			name=str(i[1])
 
 			# output utf-8 encod
 			# if output to file, it will show chinese in utf-8
 			try:
-				print name.decode('utf-8')
+				# print name.decode('utf-8')
 
 				# output path
 				stock_indep_file=os.path.join(output_folder, sid + '_' + name.decode('utf-8') )
+				str_encode='utf8'
 			except:	
-				try: 
-					print name.decode('big5')
+				print sid # + ' not encode utf-8 '
 
-					# output path
-					stock_indep_file=os.path.join(output_folder, sid + '_' + name.decode('big5') )
-				except:
-					print "Error handle " + sid
-					continue
+			try: 
+				# print name.decode('big5')
 
+				# output path
+				stock_indep_file=os.path.join(output_folder, sid + '_' + name.decode('big5') )
+				str_encode='big5'
+			except:
+				print sid # + ' not encode big5 '
 
-			print "output to ", stock_indep_file, 
+			if 'utf8' != str_encode:
+				if 'big5'!= str_encode:
+					print "ascii string " + name + name
+			
+
+			# print "output to ", stock_indep_file, 
 			try:
 				# output to file
 				f=open(stock_indep_file, "w")
@@ -56,16 +63,13 @@ for i in csv.reader(f):
 					f.write(str(i[sec_idx]) )
 					if 15 != sec_idx:
 						f.write(";")
-				else: 
-					print '...'
 				f.close()
 			except:	
 				print "Failed to write file. exit"
 				exit
 		else:
 			# ETF contains a '=' charater in id, it should be remove 
-			if sid.endswith('=', 0, 0):
-				print 'is ETF'
+			print 'is ETF' + sid
 
 
 
