078fb31cde16d9eb66d0f2e89a61525cba9e973d
Ignacs <seellk@gmail.com>
2015-05-31 23:14:13 +0800

    1. module some function 	- check if record exists in stock history file 2. remove some debug info 2. check record exist before write-in

--- dw_TSE_parser.py
+++ dw_TSE_parser.py
@@ -2,7 +2,6 @@
 import csv
 import os, sys, platform
 
-total_stock_file = open(sys.argv[1], 'r')
 # j=0
 csv_line=0
 today=''
@@ -10,6 +9,25 @@ era=''
 # 
 # def usage():
 #	echo "python dw_data_parser.py xxx.csv"
+
+################ check history file record by date ################
+def csv_date_check(f, date):
+	res=0
+	try:
+		os.stat(f)
+	except:	
+		print ""
+		return res
+
+	f=open(f, 'r')
+	for his_data in csv.reader(f):
+		if his_data[0] == str(date):
+			print "Record has inserted."
+			res=-1
+	print "return 0"
+	f.close()
+	return res
+################ check history file record by date ################
 
 # print "argv len : "+ str(len(sys.argv))
 if  3 > len(sys.argv):
@@ -28,11 +46,19 @@ except:
 #		os.makedirs(output_folder)
 #	except:
 #		print "Failed to create it."
-	exit
+
+total_stock_file = open(sys.argv[1], 'r')
 
 for nline_data in csv.reader(total_stock_file):
 	csv_line = csv_line+1
 	date_line=str(nline_data)
+	# debug 
+#	idx = 0
+#	for nbyte in date_line: 
+#		print '[' + str(idx) + ':' +  nbyte + ']',
+#		idx=idx+1
+#
+#	print date_line
 
 	# j=j+1
 	if len(nline_data) == 16:
@@ -69,27 +95,35 @@ for nline_data in csv.reader(total_stock_file):
 					continue
 			
 
+			print "output " + stock_indep_file
 			# print "output to ", stock_indep_file, 
 			try:
-				# output to file
-				f=open(stock_indep_file, "a")
+				# if record has more than 16 elements
+				if csv_date_check(stock_indep_file, today) < 0:
+					continue
+				print "begin to write"
 
+				# output to file
+				f=open(stock_indep_file, "a+")
+
+				print "start to write"
 				# write date into file as first element 
 				if '' != today:
 					f.write( today + ';')
 
 				# information begins from 2nd element
-				for sec_idx in range(2, 16):
+				for sec_idx in range(2, len(nline_data)):
 					# sperate with ";" not ','
 					f.write(str(nline_data[sec_idx]) )
 					if 15 != sec_idx:
 						f.write(";")
 					elif 15 == sec_idx:
 						f.write("\n")
+
 				f.close()
 			except:	
 				print "Failed to write file. exit"
-				exit
+				continue
 		else:
 			# ETF contains a '=' charater in id, it should be remove 
 #			if '=' ==  sid[0]:
@@ -140,22 +174,29 @@ for nline_data in csv.reader(total_stock_file):
 					str_encode='big5'
 				except:
 					print 'cant handle name ' + sid # + ' not encode big5 '
-
-			if 'utf8' != str_encode:
-				if 'big5'!= str_encode:
-					print "ascii string " + name 
-
+				#	
+				#	if 'utf8' != str_encode:
+				#		if 'big5'!= str_encode:
+				#			print "ascii string " + name 
+					continue
+			print "output " + stock_indep_file,
 			# print "output to ", stock_indep_file, 
 			try:
+				# if record has more than 16 elements
+				if csv_date_check(stock_indep_file, today) < 0:
+					continue
+				print "begin to write"
+
 				# output to file
-				f = open(stock_indep_file, "a")
+				f = open(stock_indep_file, "a+")
 
 				# write date into file as first element 
 				if '' != today:
 					f.write( today + ';')
 
+				print "start to write"
 				# information begins from 2nd element
-				for sec_idx in range(2, 16):
+				for sec_idx in range(2, len(nline_data)):
 					# sperate with ";" not ','
 					f.write(str(nline_data[sec_idx]) )
 					if 15 != sec_idx:
@@ -164,20 +205,33 @@ for nline_data in csv.reader(total_stock_file):
 						f.write("\n")
 				f.close()
 			except:	
-				print "Failed to write file. exit"
-				exit
+				continue
 
 
+	# NULL string , donothing
 	elif 0 == len(nline_data):
 		# skip NULL string 
-		print "NULL string"
+		# print "NULL string"
+		continue
+	# ROC years 
 	elif date_line[2].isdigit() and date_line[3].isdigit() and date_line[4].isdigit() and  date_line[10].isdigit() and date_line[11].isdigit() and date_line[20].isdigit() and date_line[21].isdigit():
 		# try to get first line not null : date
 		# the first line, it should be like below one:
 		# ['104\xa6~05\xa4\xeb22\xa4\xe9\xa4j\xbdL\xb2\xce\xadp\xb8\xea\xb0T']
-		print date_line[2] + date_line[3] + date_line[4] + date_line[10] + date_line[11] + date_line[20] + date_line[21]
-		era=str(int(date_line[2])*100 + int(date_line[3])*10 + int(date_line[4]) + 1911)
+		# print "[ date:  "+  date_line[2] + date_line[3] + date_line[4] + date_line[10] + date_line[11] + date_line[20] + date_line[21]+ "]"
+		era = str(int(date_line[2])*100 + int(date_line[3])*10 + int(date_line[4]) + 1911)
 		today = era + date_line[10] + date_line[11] + date_line[20] + date_line[21]
+		print "Found date string : " + today
+		continue
+	# ERA 
+	elif date_line[2].isdigit() and date_line[3].isdigit() and date_line[4].isdigit()and date_line[5].isdigit() and  date_line[18].isdigit() and date_line[19].isdigit() and date_line[32].isdigit() and date_line[33].isdigit():
+		# try to get first line not null : date
+		# the first line, it should be like below one:
+		# ['104\xa6~05\xa4\xeb22\xa4\xe9\xa4j\xbdL\xb2\xce\xadp\xb8\xea\xb0T']
+		# print "[ date:  " + date_line[2] + date_line[3] + date_line[4]+ date_line[5] +  date_line[18] + date_line[19] + date_line[32] + date_line[33]+ "]"
+		today = date_line[2] + date_line[3] + date_line[4]+ date_line[5] +  date_line[18] + date_line[19] + date_line[32] + date_line[33]
+		print "Found date string : " + today
+		continue
 
 		# for idx in range(0, len(date_line)):
 		#	print idx
