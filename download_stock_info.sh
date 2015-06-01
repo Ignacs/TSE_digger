a97290e530b539636f59ef973c83749086d25f45
Ignacs <seellk@gmail.com>
2015-06-01 22:51:20 +0800

    Input TSE data one by one

--- download_stock_info.sh
+++ download_stock_info.sh
@@ -5,9 +5,14 @@
 
 TODAY=`date +%y%m%d`
 
-python3.2 ~/roxbins/TSE_CLOSE.py 180 /media/493742f3-57ea-4deb-8a89-975caf65f8ee/lab/TSE_daily_data &> /media/493742f3-57ea-4deb-8a89-975caf65f8ee/lab/TSE_daily_data/log_$TODAY
-python3.2 ~/roxbins/OTC_CLOSE.py 180 /media/493742f3-57ea-4deb-8a89-975caf65f8ee/lab/OTC_daily_data &> /media/493742f3-57ea-4deb-8a89-975caf65f8ee/lab/OTC_daily_data/log_$TODAY
-python ~/roxbins/dw_TSE_parser.py /media/493742f3-57ea-4deb-8a89-975caf65f8ee/lab/TSE_daily_data/CLOSE/`date +%y%m%d`.csv /media/493742f3-57ea-4deb-8a89-975caf65f8ee/lab/TSE_daily_data &> /media/493742f3-57ea-4deb-8a89-975caf65f8ee/lab/TSE_daily_data/log_`date +%y%m%d`
+python3.2 ~/roxbins/TSE_CLOSE.py 180 /media/493742f3-57ea-4deb-8a89-975caf65f8ee/lab/TSE_daily_data &>> /media/493742f3-57ea-4deb-8a89-975caf65f8ee/lab/TSE_daily_data/log_$CALD_DAY.log
+python3.2 ~/roxbins/OTC_CLOSE.py 180 /media/493742f3-57ea-4deb-8a89-975caf65f8ee/lab/OTC_daily_data &>> /media/493742f3-57ea-4deb-8a89-975caf65f8ee/lab/TSE_daily_data/log_$CALD_DAY.log
+ 
+for idx in $(seq 0 180 ) 
+do 
+	CALD_DAY=`date '+%C%y%m%d' -d "$end_date+$idx days"`
+	python ~/roxbins/dw_TSE_parser.py /media/493742f3-57ea-4deb-8a89-975caf65f8ee/lab/TSE_daily_data/CLOSE/$CALD_DAY.csv /media/493742f3-57ea-4deb-8a89-975caf65f8ee/lab/TSE_daily_data &>> /media/493742f3-57ea-4deb-8a89-975caf65f8ee/lab/TSE_daily_data/log_$CALD_DAY.log
+done
 
 rsync -av  /media/493742f3-57ea-4deb-8a89-975caf65f8ee/lab/TSE_daily_data /media/data/stock_backup/TSE_daily_data
 rsync -av /media/493742f3-57ea-4deb-8a89-975caf65f8ee/lab/OTC_daily_data /media/data/stock_backup/OTC_daily_data
