fa0c0f46931ed6c5ece770f339e8fc2f080470cd
Ignacs <seellk@gmail.com>
2015-06-27 13:11:13 +0800

    Replace path (string) with variable

--- download_stock_info.sh
+++ download_stock_info.sh
@@ -17,24 +17,31 @@ echo $1
 # 	exit
 # fi 
 # 
+
+TSE_data_folder=/media/493742f3-57ea-4deb-8a89-975caf65f8ee/lab/TSE_daily_data
+OTC_data_folder=/media/493742f3-57ea-4deb-8a89-975caf65f8ee/lab/OTC_daily_data
+Stock_data_folder=/media/493742f3-57ea-4deb-8a89-975caf65f8ee/lab/stock
+backup_folder=/media/data/stock_backup/
 if [ ! "$1" == "debug" ] ; then
 	echo Normal mode 
 
 	# 
-	python3.2 ~/roxbins/TSE_CLOSE.py  $day_num /media/493742f3-57ea-4deb-8a89-975caf65f8ee/lab/TSE_daily_data >> /media/493742f3-57ea-4deb-8a89-975caf65f8ee/lab/TSE_daily_data/log_$TODAY.log
-	python3.2 ~/roxbins/OTC_CLOSE.py  $day_num /media/493742f3-57ea-4deb-8a89-975caf65f8ee/lab/OTC_daily_data >> /media/493742f3-57ea-4deb-8a89-975caf65f8ee/lab/TSE_daily_data/log_$TODAY.log
+	python3.2 ~/roxbins/TSE_CLOSE.py  $day_num  $TSE_data_folder >> $TSE_data_folder/log_$TODAY.log
+	python3.2 ~/roxbins/OTC_CLOSE.py  $day_num  $OTC_data_folder >> $OTC_data_folder/log_$TODAY.log
 	 
 	for idx in $(seq 0 $day_num ) 
 	do 
 		CALD_DAY=`date '+%C%y%m%d' -d "$end_date-$idx days"`
-		python ~/roxbins/dw_TSE_parser.py /media/493742f3-57ea-4deb-8a89-975caf65f8ee/lab/TSE_daily_data/CLOSE/$CALD_DAY.csv /media/493742f3-57ea-4deb-8a89-975caf65f8ee/lab/TSE_daily_data &>> /media/493742f3-57ea-4deb-8a89-975caf65f8ee/lab/TSE_daily_data/log_$TODAY.log
+		echo " Handle $TSE_data_folder/CLOSE/$CALD_DAY.csv " >> $TSE_data_folder/$TODAY.log
+
+		python ~/roxbins/dw_TSE_parser.py $TSE_data_folder/CLOSE/$CALD_DAY.csv $Stock_data_folder &>> $Stock_data_folder/$TODAY.log
 	done
 	
 	echo "Total $idx had check ">> ~/lab/log/dw_stock/$TODAY.log
 	
 	echo "Executed backup">> ~/lab/log/dw_stock/$TODAY.log
-	rsync -av  /media/493742f3-57ea-4deb-8a89-975caf65f8ee/lab/TSE_daily_data /media/data/stock_backup/TSE_daily_data>> ~/lab/log/dw_stock/$TODAY.log
-	rsync -av /media/493742f3-57ea-4deb-8a89-975caf65f8ee/lab/OTC_daily_data /media/data/stock_backup/OTC_daily_data>> ~/lab/log/dw_stock/$TODAY.log
+	rsync -av  $TSE_data_folder $backup_folder >> $backup_folder/$TODAY.log
+	rsync -av $OTC_data_folder $backup_folder >>  $backup_folder/$TODAY.log
 else 
 	echo "debug mode"
 	python3.2 TSE_CLOSE.py $day_num output/
