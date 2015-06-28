7b9e8a6e7c895c807a6b33ffeefcf7523434912f
Ignacs <seellk@gmail.com>
2015-06-28 21:37:48 +0800

    - Add more example

--- module_template.py
+++ module_template.py
@@ -7,35 +7,6 @@ con = None
 idx_DB=1
 db_postfix=".sl3"
  
-# table format 
-#		Date INT,
-#		Stock_number INT, 
-#		Deal_volumn INT,
-#		Deal_money INT,
-#		Open REAL, 
-#		HIGHEST REAL,
-#		LOWEST REAL,
-#		CLOSE REAL,
-#		BID_PRICE REAL, 
-#		BUY_VOL INT, 
-#		SELL_PRICE REAL,
-#		SELL_VOL INT,
-#		PE INT
-col_list = [ 
-	'idx_Date',
-	'idx_Stock_number',
-	'idx_Deal_volumn',
-	'idx_Deal_money',
-	'idx_Open',
-	'idx_HIGHEST',
-	'idx_LOWEST',
-	'idx_CLOSE',
-	'idx_Bid_price',
-	'idx_BUY_VOL',
-	'idx_SELL_price',
-	'idx_SELL_VOL',
-	'idx_PE' ]
-
 ##########################################
 # arguments check 
 if len(sys.argv) < 2:
@@ -62,6 +33,21 @@ long_calDateNum=108
 mid_calDateNum=36
 short_calDateNum=12
 
+# table format 
+#		Date INT,
+#		Stock_number INT, 
+#		Deal_volumn INT,
+#		Deal_money INT,
+#		Open REAL, 
+#		HIGHEST REAL,
+#		LOWEST REAL,
+#		CLOSE REAL,
+#		BID_PRICE REAL, 
+#		BUY_VOL INT, 
+#		SELL_PRICE REAL,
+#		SELL_VOL INT,
+#		PE INT
+
 # The element to query
 Query_item=[
 	'Date',
@@ -79,6 +65,15 @@ with con:
 		print record[1], 
 		print record[2], 
 		print record[3] 
+
+	print "============================================"
+	print "Show short-term data reverse"
+	for record in cur.execute("select " + Query_item[0] + "," + Query_item[1] + "," + Query_item[2]+ "," + Query_item[3] +" from stock order by Date desc limit " + str(short_calDateNum)):
+		print record[0], 
+		print record[1], 
+		print record[2], 
+		print record[3] 
+
 
 	# cur.execute("select * from stock where date = " + str(nline_data[0]) ):
 	# cur.execute("select * from stock where date=20150628" ):
