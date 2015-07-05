b2e05c5732c25d2fb191c19be545f3baec3e9297
Ignacs <seellk@gmail.com>
2015-07-05 20:05:02 +0800

    return result list

--- list_all_files.py
+++ list_all_files.py
@@ -3,7 +3,6 @@
 import glob, os, sys, platform, subprocess
 from os import listdir
 from os.path import isfile, join
-from findtools.find_files import (find_files, Match)
 
 #import module_PriceAVG 
 #import module_template
@@ -13,39 +12,38 @@ from findtools.find_files import (find_files, Match)
 # module_PriceAVG.module_func(['/home/rox/lab/stock/TSE_dwrobot/db_test/0050.sl3'])
 #arg='/home/rox/lab/stock/TSE_dwrobot/db_test/0050.sl3'
 #folder='/home/rox/lab/git_test/TSE_robot/TSE_dwrobot'
-folder='/home/ignacs/lab/git_test/TSE_robot/TSE_dwrobot'
-dbFile_filter='*.sl3'
+# folder='/home/ignacs/lab/git_test/TSE_robot/TSE_dwrobot'
 
 ############# function to get paht of all files under folder #############
-def get_filepaths(directory):
-	"""
-	This function will generate the file names in a directory 
-	tree by walking the tree either top-down or bottom-up. For each 
-	directory in the tree rooted at directory top (including top itself), 
-	it yields a 3-tuple (dirpath, dirnames, filenames).
-	"""
-	file_paths = []  # List which will store all of the full filepaths.
+def get_filepaths(directory, subname):
+    """
+    This function will generate the file names in a directory 
+    tree by walking the tree either top-down or bottom-up. For each 
+    directory in the tree rooted at directory top (including top itself), 
+    it yields a 3-tuple (dirpath, dirnames, filenames).
+    """
+    file_paths = []  # List which will store all of the full filepaths.
+    print("Handle " + str(directory) + "...")
 
-	# Walk the tree.
-	for root, directories, files in os.walk(directory):
-		for filename in files:
-			# Join the two strings in order to form the full filepath.
-				filepath = os.path.join(root, filename)
-		file_paths.append(filepath)  # Add it to the list.
-	return file_paths  # Self-explanatory.
-
+    # Walk the tree.
+    for root, directories, files in os.walk(directory):
+        for filename in files:
+            # Join the two strings in order to form the full filepath.
+            filepath = os.path.join(root, filename)
+            file_paths.append(filepath)  # Add it to the list.
+    return file_paths  # Self-explanatory.
 
 ############# function to list all files #############
 def list_files(path):
-	''' function to list all files under some path
-		'''
-	# returns a list of names (with extension, without full path) of all files 
-	# in folder path
-	files = []
-	for name in os.listdir(path):
-		if os.path.isfile(os.path.join(path, name)):
-			files.append(name)
-	return files 
+    ''' function to list all files under some path
+            '''
+    # returns a list of names (with extension, without full path) of all files 
+    # in folder path
+    files = []
+    for name in os.listdir(path):
+            if os.path.isfile(os.path.join(path, name)):
+                    files.append(name)
+    return files 
 
 
 ################################################################################
@@ -56,14 +54,12 @@ def list_files(path):
 
 
 # Run the above function and store its results in a variable.   
-#for foundFiles in get_filepaths(folder):
+# for foundFiles in get_filepaths(folder):
 #	print (foundFiles)
 
 
 # func can filter subname
 # Recursively find all *.sh files in **/usr/bin**
-sh_files_pattern = Match(filetype='f', name=dbFile_filter)
-for found_file in find_files(path=folder, match=sh_files_pattern):
-	print (found_file)
+# sh_files_pattern = Match(filetype='f', name=dbFile_filter)
 
-sys.exit()
+# sys.exit()
